42f0e3c927cb93dbba1a16972c1bb8dee8b9f30b
Mathis845 <75135457+Mathis845@users.noreply.github.com>
2021-05-06 16:54:41 +0200

    Add files via upload
    Les variables sont renommés + Début de documentation + Score + Collision avec les raquettes

--- Projet_Pong/pong.py
+++ Projet_Pong/pong.py
@@ -18,66 +18,86 @@ screen = pygame.display.set_mode([longueur,largeur])
 
 screen.fill([0,0,0])
 
-x=500
-y=333
-r=20
-X=8
-Y=12
+x=500 # abcisse du centre de la balle
+y=333 # ordonnée du centre de la balle
+r=20  # rayon de la balle
+vx=8   #vitesse dans la direction des x positifs
+vy=12  # vitesse dans la direction des y positifs
 
 phrase="".join(lst)
 police = pygame.font.Font("police.ttf", 128)
 texte = police.render((phrase), True, (255, 255, 255))
 
-rx=30
-r2x=longueur-30
-ry=largeur/2
-r2y=largeur/2
-dry=5
-rh=100
-rl=20
-d=10
+raquette_x=30
+raquette2_x=longueur-30
+raquette_y=largeur/2
+raquette2_y=largeur/2
+raquette_h=100
+raquette_l=20
+deplacement=10
+marge = 2*r
+
 pygame.key.set_repeat(10, 10)
+
 while True:
     screen.fill([0,0,0])
-    s1=str(score1)
-    s2=str(score2)
     lst=[s1," ",":"," ",s2]
+    phrase="".join(lst)
     texte = police.render((phrase), True, (255, 255, 255))
+    
     pygame.draw.circle(screen, couleur, [x,y], r, 0)
-    pygame.draw.rect(screen, couleur, [rx,ry,rl,rh])
-    pygame.draw.rect(screen, couleur, [r2x,r2y,rl,rh])
+    pygame.draw.rect(screen, couleur, [raquette_x,raquette_y,raquette_l,raquette_h])
+    pygame.draw.rect(screen, couleur, [raquette2_x,raquette2_y,raquette_l,raquette_h])
+    
     screen.blit(texte,(longueur/2-130,30))
-    x=x+X
-    y=y+Y
+    
+    x=x+vx
+    y=y+vy
+    
+    #collision avec les murs
     if x+r > longueur:
-        X= -X
-        score1+=1
+        vx= -vx
+        score1=score1+1
     if x-r < 0:
-        X= -X
-        score2+=1
+        vx= -vx
+        score2=score2+1        
     if y+r > largeur:
-        Y= -Y
+        vy= -vy        
     if y-r < 0:
-        Y= -Y
+        vy= -vy
+    s1=str(score1)
+    s2=str(score2)   
+    
     pygame.display.flip()
     pygame.time.delay(15)
+    
+    #collision entre le bord droit de la raquette de gauche (uniquement) et la balle (plus une marge)
+    if x-r < raquette_x + raquette_l  and y > raquette_y - marge and y  < raquette_y + raquette_h + marge:
+        vx = -vx
+        
+    #collision entre le bord gauche de la raquette de droite (uniquement) et la balle (plus une marge)
+    if x + r > raquette2_x and y > raquette2_y - marge and y  < raquette2_y + raquette_h + marge:
+        vx = -vx
+
+
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.display.quit()
             sys.exit()
+            
         if event.type==pygame.KEYDOWN:
             if pygame.key.get_pressed()[pygame.K_w]:
-                if ry>0:
-                    ry=ry-d
+                if raquette_y>0:
+                    raquette_y=raquette_y-deplacement
             if pygame.key.get_pressed()[pygame.K_s]:
-                if ry+rh<largeur:
-                    ry=ry+d
+                if raquette_y+raquette_h<largeur:
+                    raquette_y=raquette_y+deplacement
             if pygame.key.get_pressed()[pygame.K_UP]:
-                if r2y>0:
-                    r2y=r2y-d
+                if raquette2_y>0:
+                    raquette2_y=raquette2_y-deplacement
             if pygame.key.get_pressed()[pygame.K_DOWN]:
-                if r2y+rh<largeur:
-                    r2y=r2y+d
+                if raquette2_y+raquette_h<largeur:
+                    raquette2_y=raquette2_y+deplacement
             
             
             
